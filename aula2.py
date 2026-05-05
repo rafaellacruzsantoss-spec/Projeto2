349d137929a02d7d42152b4eb86d436a2a47077f
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-05-05 15:40:38 -0300

    Update aula2.py

--- aula2.py
+++ aula2.py
@@ -1,26 +1,10 @@
-import streamlit as st
-import pandas as pd
-st.title("Política Aplicada")
-df = pd.read_csv('deputados_2022.csv')
 
-sigla = st.text_input('Digite a sigla do partido')
-uf = st.text_input('Digite a UF')
-
-if sigla:
-    df_filtrado = df[df['partido']==sigla.upper()]
-else:
-    df_filtrado = df
-
-if uf:
-    df_filtrado = df_filtrado[df_filtrado["uf"] == uf.upper()]
-
-st.dataframe(df_filtrado)
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 
 # --- Carregamento dos dados ---
-uploaded_file = st.file_uploader("Suba seu CSV", type="csv")
+uploaded_file = st.file_uploader("deputados_2022.csv", type="csv")
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
