9384739da293888bcddfdb04fb84d46925a68fdd
rafaellacruzsantoss-spec <rafaellacruzsantoss@gmail.com>
2026-05-05 15:24:36 -0300

    Update aula2.py

--- aula2.py
+++ aula2.py
@@ -15,3 +15,24 @@ if uf:
     df_filtrado = df_filtrado[df_filtrado["uf"] == uf.upper()]
 
 st.dataframe(df_filtrado)
+# --- CRIAÇÃO DO GRÁFICO DE PIZZA ---
+    
+    st.subheader(f"Distribuição para o {partido_selecionado}")
+
+    # Pergunta: Qual coluna você quer contar para o gráfico? 
+    # Exemplo: 'genero', 'cargo' ou 'estado'
+    coluna_para_pizza = st.selectbox(
+        "Ver proporção de:", 
+        options=df_filtrado.columns
+    )
+
+    # Criando o gráfico com Plotly
+    fig = px.pie(
+        df_filtrado, 
+        names=coluna_para_pizza, 
+        title=f"Proporção de {coluna_para_pizza} no {partido_selecionado}",
+        hole=0.3 # Opcional: transforma em gráfico de rosca
+    )
+
+    # Exibindo no Streamlit
+    st.plotly_chart(fig)
